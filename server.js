446f5428b34705a590ab84a911807d5f04d877aa
chiefprist64 <chiefprist64@gmail.com>
2024-12-10 09:53:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "chiefprist64@gmail.com";
 //const pass = "ihixgiertsquakyr";
-// 3rd nov
+// 3rd dec
 
 // Middleware
 app.use(express.json());
